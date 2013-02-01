30095cb671eed844c11dadc5d662094427345490
Ben Dennis <ben@dennistech.com>
2013-02-01 14:11:20 -0500

    Initial Commit

---
+++ HSEHS/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  HSEHS
+//
+//  Created by Ben Dennis on 2/1/13.
+//  Copyright (c) 2013 Dennis Tech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HSEHS/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HSEHS
+//
+//  Created by Ben Dennis on 2/1/13.
+//  Copyright (c) 2013 Dennis Tech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
